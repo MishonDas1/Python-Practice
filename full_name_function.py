2e6756d72de1b366b327134655d1fac49cbc5926
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-09-21 03:18:12 +0600

    Add full_name function to concatenate first and last names

---
+++ full_name_function.py
@@ -0,0 +1,11 @@
+def full_name(f_name, l_name):
+    # Concatenate strings with a space
+    full = f_name + " " + l_name
+    return full
+
+f_name = "Mishon"
+l_name = "Das"
+fullname = full_name(f_name, l_name)
+
+# Display output to the console
+print(fullname)
