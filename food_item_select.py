340417d5c2cbf982ecc4b3f8d2e866e073c31336
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-09-21 03:12:50 +0600

    Add food item selection functionality

---
+++ food_item_select.py
@@ -0,0 +1,16 @@
+#if-elif-else
+
+# Prompt the user and store the input
+foodno= int(input("Enter your food item no:"))
+if foodno == 101:
+# Display output to the console
+  print("This is Pizza")
+elif foodno ==200:
+# Display output to the console
+  print("This is Burger")
+elif foodno ==300:
+# Display output to the console
+  print("This is Sandwitch")
+else:
+# Display output to the console
+  print("No item found")
