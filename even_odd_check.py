48ac2224ba2cef33b42d453b70394c2e5e23f15d
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-09-21 03:11:50 +0600

    Add even-odd check functionality

---
+++ even_odd_check.py
@@ -0,0 +1,10 @@
+x=12
+if x%2 == 0:
+# Display output to the console
+  print("even")
+  print ("Not ODD number")
+else:
+# Display output to the console
+  print("odd")
+# Display output to the console
+  print("Not Even Number")
