f560ae701a13a003ecfdec4f024d7ac4b2a55f7b
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-09-21 03:09:07 +0600

    Add user input for first and last name

---
+++ Basic.py
@@ -0,0 +1,6 @@
+# Prompt the user and store the input
+fname =input("Enter your first name:")
+lname =input ("Enter your last name:")
+# Concatenate strings or add values
+fullname = fname + " " + lname
+print (fullname)
