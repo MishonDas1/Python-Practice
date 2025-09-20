2daca8bcbf452993c5a5d9a1e769d38f2c591529
Mishon Das <mishon.da@uttarauniversity.edu.bd>
2025-09-21 03:16:13 +0600

    Implement for loops for even numbers and countdown

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+#for loop
+i =0
+
+for i in range(0,10,2):
+# Display output to the console
+  print(i, end = " ")
+# Display output to the console
+  print("\n")
+  j=0
+for j in range (10,0,-1):
+# Display output to the console
+  print(j,end = " ")
